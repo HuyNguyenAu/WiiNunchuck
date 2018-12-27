066feadfa2221ce7de8352e1fbb69bc6beba6d14
HuyNguyenAu <huynguyenau@gmail.com>
2018-12-28 00:28:15 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from nunchuck import nunchuck
 import time
 
-lowerBound = 0
-center = 128
-upperBound = 255 
-dead = 10
+lowerBound = 0.0
+center = 128.0
+upperBound = 255.0 
+dead = 10.0
 deadband = [center - dead, center + dead]
 wii = nunchuck()
 
@@ -15,13 +15,13 @@ def getValue():
   value = getRaw()
   # Deadband [118, 138].
   if value >= deadband[0] and value <= deadband[1]:
-    return 0
+    return 0.0
   # Positive (138, 255].
   elif value > deadband[1] and value <= upperBound:  
-    return ((value - deadband[1]) / (upperBound - deadband[1]))
+    return round((value - deadband[1]) / (upperBound - deadband[1]), 1)
   # Negative [0, 118).
   elif value >= lowerBound and value < deadband[0]:  
-    return (-1 * ((deadband[0] - value) / deadband[0]))
+    return round(-1.0 * ((deadband[0] - value) / deadband[0]), 1)
   return 0
 
 def main():
