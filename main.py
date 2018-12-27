21dcab9ebe4a534a8bb1936d92b9c393bd00f1a6
HuyNguyenAu <huynguyenau@gmail.com>
2018-12-27 23:56:22 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from nunchuck import nunchuck
 import time
 
+lowerBound = 0
+center = 128
+upperBound = 255 
+dead = 10
+deadband = [center - dead, center + dead]
 wii = nunchuck()
-lowerBand = [0, 20]
-upperBand = [235, 255]
-deadBand = [100, 128, 148] 
 
+# Raw output data from nunchuck.
+def getRaw():
+  return wii.getJoystickY()
+
+# Convert raw data.
 def getValue():
-  value = wii.getJoystickY()
-  return value
+  value = getRaw()
+  # Deadband [118, 138].
+  if value >= deadband[0] and value <= deadband[1]:
+    return 0
+  # Positive (138, 255].
+  elif value > deadband[1] and value <= upperBound:  
+    return (value / (upperBound - deadband[1]))
+  # Negative [0, 118).
+  elif value >= lowerBound and value < deadband[0]:  
+    return -1 * ((deadband[0]) - value / deadband[0])
+  return 0
 
 def main():
  while True:
