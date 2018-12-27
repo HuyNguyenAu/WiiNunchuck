56a6f6ae568a31cddaa92edfa56b0172cf0198ef
HuyNguyenAu <huynguyenau@gmail.com>
2018-12-27 19:15:59 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ lowerBound = 34
 upperBound = 239
 neutral = 136
 
+# TODO 255 and 0 come up randomly?
+value_prev = 0
+
 def getValue():
   value = wii.getJoystickY()
   if (value >= lowerBound and value <= upperBound):
+    global value_prev
+    value_prev = value
     return value
+  else:
+    return value_prev
 
 def main():
  while True:
