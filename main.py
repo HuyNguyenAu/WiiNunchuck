1f2d00be3d5feb4a7216aaf496b13a61e6b7eca0
HuyNguyenAu <huynguyenau@gmail.com>
2018-12-27 16:14:42 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Code adapted from:
+# 1. https://www.hackster.io/infusion/using-a-wii-nunchuk-with-arduino-597254
+# 2. https://github.com/Boeeerb/Nunchuck/blob/master/Raspberry%20Pi/nunchuck.py
+
+from smbus import SMBus
+import time
+
+dev_bus = 1
+dev_addr = 0x52
+delay = 0.1
+bus = SMBus(dev_bus)
+
+def getJoystickX():
+    bus.write_byte_data(dev_addr, 0x00)
+    time.sleep(delay)
+    return bus.read_byte(dev_addr)
+
+def init():
+    bus.write_byte_data(dev_addr, 0xF0, 0x55)
+    bus.write_byte_data(dev_addr, 0xFB, 0x00)
+    time.sleep(delay)
